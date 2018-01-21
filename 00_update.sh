e9e8a9c8fe6d191d687e3cffd50b9946dae3db21
koobe <koobe@koobes-MBP.home>
2018-01-21 20:01:49 +0000

    Initial commit.
    Update OS and install needed packages.

---
+++ 00_update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -uexo pipefail
+
+# Update and install required packages
+apt update -y
+apt upgrade -y
+
+apt install -y wireless-tools \
+               wpasupplicant  \
+               openssh-server \
+               iotop          \
+               htop           \
+               vim            \
+               tree           \
+               qemu-kvm       \
+               libvirt-bin    \
+               bridge-utils   \
+               hugepages      \
+               qemu-utils     \
+               ovmf
+
+
