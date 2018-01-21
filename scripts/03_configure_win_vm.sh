a3b018f93d7f0bbe58720df204b6b8f3d69a157a
koobe <>
2018-01-21 22:39:35 +0000

    Configure quemu

--- scripts/03_configure_win_vm.sh
+++ scripts/03_configure_win_vm.sh
@@ -6,4 +6,6 @@ install --owner root --group root --mode 0600 ../files/win10_kvm/win10_VARS.fd /
 
 install --owner root --group root --mode 0600 ../files/win10_kvm/vga.rom /var/lib/libvirt/images/vga.rom
 
+install --owner root --group root --mode 0600 ../files/win10_kvm/qemu.conf /etc/libvirt/qemu.conf
+
 virsh define ../files/win10_kvm/win10_kvm.xml
